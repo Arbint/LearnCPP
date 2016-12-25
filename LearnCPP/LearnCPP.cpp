d7a2d016a89e096cbc83c3e31bc153afda11a4c1
Arbint <cg.jingtianli@gmail.com>
2016-12-24 18:30:20 -0600

    did some comments on it

--- LearnCPP/LearnCPP.cpp
+++ LearnCPP/LearnCPP.cpp
@@ -11,15 +11,19 @@ constexpr double pi = 22 / 7;
 
 int main()
 {	
-	std::string gaga = "gaga";
-	int x;
-	std::cin >> x;
-	char xToChar = x;
-	
-	gaga += x;
-	io::printWithEndl(xToChar);
-	io::printWithEndl(gaga);
-	io::printWithEndl("wht");
+	double d = 2.5;
+	int i = 2;
+	double d2 = d / i; // double/ int = double/(double(int)), so d2 = 1.25 
+	int i2 = d / i; // i2 is an int, so if int = double, int = int(double), so i2 = 1
+	int i3( d / i ); // error: double -> int conversion may narrow
+
+	d2 = d / i;
+	i2 = d / i;
+
+	io::printWithEndl(i);
+	io::printWithEndl(i2);
+	io::printWithEndl(i3);
+	io::printWithEndl(d2);
 	return 0;
 }
 
